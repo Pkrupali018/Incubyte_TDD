902b9d13d66780dad565fc6e25bad3194f8460bc
Pkrupali018 <pkrupali018@gmail.com>
2023-08-04 14:19:49 +0530

    pass test for user define coordinates

--- test/test.py
+++ test/test.py
@@ -9,6 +9,8 @@ class CoordinatesTestCase(unittest.TestCase):
         coordinate = Coordinates()
         result = coordinate.add_coordinates()
         self.assertEqual([0, 0, 0, 'N'], result)
+        result_with_ip = coordinate.add_coordinates(1, 0, 2, 'U')
+        self.assertEqual([1, 0, 2, 'U'], result_with_ip)
 
 
 if __name__ == '--main__':
