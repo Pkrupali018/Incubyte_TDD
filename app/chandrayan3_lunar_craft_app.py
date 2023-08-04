35a199410fe7286f4ad596de2ea94cffb5ad2e8b
Pkrupali018 <pkrupali018@gmail.com>
2023-08-04 15:41:20 +0530

    add code for Move function and pass the test case

--- app/chandrayan3_lunar_craft_app.py
+++ app/chandrayan3_lunar_craft_app.py
@@ -5,14 +5,40 @@ class Coordinates:
         self.coordinates = [0, 0, 0, 'N']
 
     # Add coordinates use when user wants to enter their own choice coordinates.
-    def add_coordinates(self, e_w='', n_s='', a_b='', ini_direction=''):
-        if e_w != '':
-            self.coordinates = [e_w, n_s, a_b, ini_direction]
+    def add_coordinates(self, ip_coordinates=''):
+        if ip_coordinates != '':
+            self.coordinates = ip_coordinates
         return self.coordinates
 
     # Get coordinates method useful when we will deal with other functionality of lunar carft like MOVE
     def get_coordinates(self):
         return self.coordinates
+
+
+class Move:
+    def __int__(self):
+        self.after_forward = []
+        self.after_backward = []
+
+    def forward(self, input_coordinates):
+        if input_coordinates[3] == 'E' or input_coordinates[3] == 'W':
+            input_coordinates[0] += 1
+        elif input_coordinates[3] == 'N' or input_coordinates[3] == 'S':
+            input_coordinates[1] += 1
+        elif input_coordinates[3] == 'U' or input_coordinates[3] == 'D':
+            input_coordinates[2] += 1
+            # coordinate.add_coordinates(self.coordinates[0], self.coordinates[1], self.coordinates[2], self.coordinates[3])
+        return input_coordinates
+
+    def backward(self, input_coordinates):
+        if input_coordinates[3] == 'E' or input_coordinates[3] == 'W':
+            input_coordinates[0] -= 1
+        elif input_coordinates[3] == 'N' or input_coordinates[3] == 'S':
+            input_coordinates[1] -= 1
+        elif input_coordinates[3] == 'U' or input_coordinates[3] == 'D':
+            input_coordinates[2] -= 1
+        # coordinate.add_coordinates(self.coordinates[0], self.coordinates[1], self.coordinates[2], self.coordinates[3])
+        return input_coordinates
 
 
 if __name__ == '__main__':
@@ -29,10 +55,31 @@ if __name__ == '__main__':
         north_south = int(input("Enter value for north or south from origin: "))
         above_below = int(input("Enter value for above or below from origin: "))
         initial_direction = input("Enter value for initial direction: ")
-        result = coordinate.add_coordinates(east_west, north_south, above_below, initial_direction)
+        input_coordinates = [east_west, north_south, above_below, initial_direction]
+        result = coordinate.add_coordinates(input_coordinates)
     else:
         result = coordinate.add_coordinates()
 
-    n = len(result)
+    no_of_commands = int(input("Enter the number how many commands you want to give: "))
+    commands = []
+
+    for i in range(0, no_of_commands):
+        ele = input("Enter next command: ")
+        commands.append(ele)
+
+    for i in range(0, no_of_commands):
+        if commands[i] == 'f':
+            move = Move()
+            coordinates = coordinate.get_coordinates()
+            after_forward = move.forward(coordinates)
+            coordinate.add_coordinates(after_forward)
+        elif commands[i] == 'b':
+            move = Move()
+            coordinates = coordinate.get_coordinates()
+            after_backward = move.backward(coordinates)
+            coordinate.add_coordinates(after_backward)
+
+    check = coordinate.get_coordinates()
+    n = len(check)
     for i in range(0, n):
-        print(result[i])
+        print(check[i])
--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app.chandrayan3_lunar_craft_app import Coordinates
+from app.chandrayan3_lunar_craft_app import Coordinates, Move
 
 
 class CoordinatesTestCase(unittest.TestCase):
@@ -10,16 +10,16 @@ class CoordinatesTestCase(unittest.TestCase):
         coordinate = Coordinates()
         coordinates = coordinate.add_coordinates()
         self.assertEqual([0, 0, 0, 'N'], coordinates)
-        coordinates_with_ip = coordinate.add_coordinates(1, 0, 2, 'U')
+        coordinates_with_ip = coordinate.add_coordinates([1, 0, 2, 'U'])
         self.assertEqual([1, 0, 2, 'U'], coordinates_with_ip)
 
     # Test case to verify the functionality of Move like forward and backward
     def test_move(self):
         move = Move()
-        after_forward = move.forward(0, 0, 0, 'N')
+        after_forward = move.forward([0, 0, 0, 'N'])
         self.assertEqual([0, 1, 0, 'N'], after_forward)
-        after_backward = move.backword(0, 0, 0, 'N')
-        self.assertEqual([0, -1, 0, 0], after_backward)
+        after_backward = move.backward([0, 0, 0, 'N'])
+        self.assertEqual([0, -1, 0, 'N'], after_backward)
 
 
 if __name__ == '--main__':
