d3483bc2f733de2059f2015c8d551caf716d0217
Pkrupali018 <pkrupali018@gmail.com>
2023-08-04 14:32:21 +0530

    Write test case and fail for new functionality Move

--- test/test.py
+++ test/test.py
@@ -7,10 +7,17 @@ class CoordinatesTestCase(unittest.TestCase):
 
     def test_coordinates(self):
         coordinate = Coordinates()
-        result = coordinate.add_coordinates()
-        self.assertEqual([0, 0, 0, 'N'], result)
-        result_with_ip = coordinate.add_coordinates(1, 0, 2, 'U')
-        self.assertEqual([1, 0, 2, 'U'], result_with_ip)
+        coordinates = coordinate.add_coordinates()
+        self.assertEqual([0, 0, 0, 'N'], coordinates)
+        coordinates_with_ip = coordinate.add_coordinates(1, 0, 2, 'U')
+        self.assertEqual([1, 0, 2, 'U'], coordinates_with_ip)
+
+    def test_move(self):
+        move = Move()
+        after_forward = move.forward(0, 0, 0, 'N')
+        self.assertEqual([0, 1, 0, 'N'], after_forward)
+        after_backword = move.backword(0, 0, 0, 'N')
+        self.assertEqual([0, -1, 0, 0], after_backword)
 
 
 if __name__ == '--main__':
