3869ddc89fa75941f32571af861610c0e2e0e21b
Pkrupali018 <pkrupali018@gmail.com>
2023-08-04 14:49:21 +0530

    add get_coordinate method in Coordinate class and add comments

--- app/chandrayan3_lunar_craft_app.py
+++ app/chandrayan3_lunar_craft_app.py
@@ -1,25 +1,29 @@
 class Coordinates:
 
     def __init__(self):
-        self.east_west = 0
-        self.north_south = 0
-        self.above_below = 0
-        self.initial_direction = 'N'
+        # coordinates list contains east_west, north_south, above_below and initial_direction in order.
+        self.coordinates = [0, 0, 0, 'N']
 
+    # Add coordinates use when user wants to enter their own choice coordinates.
     def add_coordinates(self, e_w='', n_s='', a_b='', ini_direction=''):
-        coordinates = []
         if e_w != '':
-            coordinates = [e_w, n_s, a_b, ini_direction]
-        else:
-            coordinates = [self.east_west, self.north_south, self.above_below, self.initial_direction]
-        return coordinates
+            self.coordinates = [e_w, n_s, a_b, ini_direction]
+        return self.coordinates
+
+    # Get coordinates method useful when we will deal with other functionality of lunar carft like MOVE
+    def get_coordinates(self):
+        return self.coordinates
 
 
 if __name__ == '__main__':
+
+    # Create object of coordinate class
     coordinate = Coordinates()
 
-    status = input("If you want to add your own coordinates then write yes otherwise use default coordinates by writhing no: ")
+    status = input("If you want to add your own coordinates then write yes otherwise use default coordinates by "
+                   "writhing no: ")
 
+    # If user want to enter their own choice coordinates then use this
     if status == "yes":
         east_west = int(input("Enter value for east or west from origin: "))
         north_south = int(input("Enter value for north or south from origin: "))
--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ from app.chandrayan3_lunar_craft_app import Coordinates
 
 class CoordinatesTestCase(unittest.TestCase):
 
+    # Test case for coordinated which is by default origin or user defined
     def test_coordinates(self):
         coordinate = Coordinates()
         coordinates = coordinate.add_coordinates()
@@ -12,12 +13,13 @@ class CoordinatesTestCase(unittest.TestCase):
         coordinates_with_ip = coordinate.add_coordinates(1, 0, 2, 'U')
         self.assertEqual([1, 0, 2, 'U'], coordinates_with_ip)
 
+    # Test case to verify the functionality of Move like forward and backward
     def test_move(self):
         move = Move()
         after_forward = move.forward(0, 0, 0, 'N')
         self.assertEqual([0, 1, 0, 'N'], after_forward)
-        after_backword = move.backword(0, 0, 0, 'N')
-        self.assertEqual([0, -1, 0, 0], after_backword)
+        after_backward = move.backword(0, 0, 0, 'N')
+        self.assertEqual([0, -1, 0, 0], after_backward)
 
 
 if __name__ == '--main__':
